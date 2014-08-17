2ccee70fad5aaf5f6aff8f4b81c354413cb60988
Foulgit <yumolun@hotmail.com>
2014-08-17 18:37:32 +0200

    first push

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<title>Molen's site</title>
+	<link rel="shortcut icon" href="favicon.ico">
+	<link href="css/home.css?version=<?=md5_file('css/home.css')?>" rel="stylesheet" type="text/css" />
+	<!-- html5.js for IE less than 9 -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- css3-mediaqueries.js for IE less than 9 -->
+	<!--[if lt IE 9]>
+		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<div id="container">
+		<header></header>
+		<div id="metro">
+			<ul class="grid">
+				<li><a href="http://blog.yumolun.com">Blog</a></li>
+				<li><a href="https://www.linkedin.com/profile/view?id=248297652">LinkedIn</a></li>
+				<li>Contact</li>
+				<li>Test</li>
+			</ul>
+		</div>
+	</div>
+	<script type="text/javascript" src="js/home.js?version=<?=md5_file('js/home.js')?>"></script>
+</body>
+</html>
