25c550739933a5be62b46e0103f63695500ecaef
Foulgit <yumolun@hotmail.com>
2014-08-24 23:41:20 +0200

    site is changed

--- index.php
+++ index.php
@@ -1,59 +1,39 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="This is yumolun's website.余默伦的个人网站"/>
-	<meta name="keywords" content="yumolun,molen,molun yu,余默伦,blog"/>
-	<title>Molen's</title>
-	<link rel="shortcut icon" href="favicon.ico">
-	<link href="css/index.css?version=<?=md5_file('css/index.css')?>" rel="stylesheet" type="text/css" />
-	<!-- html5.js for IE less than 9 -->
-	<!--[if lt IE 9]>
-		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="This is yumolun's website.余默伦的个人网站"/>
+  <meta name="keywords" content="yumolun,molen,molun yu,余默伦,blog"/>
+  <title>Molen's</title>
+  <link rel="shortcut icon" href="favicon.ico">
+  <link href="css/index.css?version=<?=md5_file('css/index.css')?>" rel="stylesheet" type="text/css" />
+  <!-- html5.js for IE less than 9 -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
 
-	<!-- css3-mediaqueries.js for IE less than 9 -->
-	<!--[if lt IE 9]>
-		<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
-	<![endif]-->
+  <!-- css3-mediaqueries.js for IE less than 9 -->
+  <!--[if lt IE 9]>
+    <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+  <![endif]-->
 </head>
 <body>
-	<div id="container">
-		<header></header>
-		<div id="metro">
-			<ul class="grid">
-				<li id="grid1" class="grid1">Blog</li>
-				<li id="grid2" class="grid2">LinkedIn</li>
-				<li id="grid3" class="grid3">Contact</li>
-				<li id="grid4" class="grid4">Favourites</li>
-			</ul>
-		</div>
-		<div id="contact" class="grid3">
-			<span class="close">close</span>
-			<table>
-				<tr>
-					<td>
-						<span>signupmyu#163.com</span><br>
-						<span>1234567890</span>
-					</td>
-				</tr>
-			</table>
-		</div>
-		<div id="favourites" class="grid4">
-			<span class="close">close</span>
-			<table>
-				<tr>
-					<td>
-						<span><a href="http://jsfiddle.net" target="_blank">JSFiddle</a></span><br>
-						<span><a href="http://www.regexper.com/" target="_blank">Regexper</a></span><br>
-						<span><a href="http://responsivetest.net/" target="_blank">Responsive test</a></span>
-					</td>
-				</tr>
-			</table>
-		</div>
-	</div>
-	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-	<script type="text/javascript" src="js/index.js?version=<?=md5_file('js/index.js')?>"></script>
+  <div id="container">
+    <header></header>
+    <div id="content">  
+      <div id="logo">M</div>
+      <div id="welcome">Welcome to Molen's</div>
+      <div class="main-nav">
+        <ul class="nav-bar">
+          <li><a href="http://blog.yumolun.com" target="_blank">Blog</a></li>
+          <li><a href="">Resume</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </div>
+    </div>-->
+  </div>
+  <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="js/index.js?version=<?=md5_file('js/index.js')?>"></script>
 </body>
 </html>
--- js/index.js
+++ js/index.js
@@ -1,38 +1,3 @@
 $(document).ready(function(){
-	var $contact = $("#contact");
-	var $favourites = $("#favourites");
-	var $metro = $("#metro");
-	var $grid1 = $("#grid1");
-	var $grid2 = $("#grid2");
-	var $grid3 = $("#grid3");
-	var $grid4 = $("#grid4");
-	var $close = $(".close");
-
-	$contact.add($favourites).css("height", $metro.height());
-
-	$grid1.click(function(){
-		window.open("http://blog.yumolun.com");
-	});
-	$grid2.click(function(){
-		window.open("https://www.linkedin.com/profile/view?id=248297652");
-	});
-	$grid3.click(function(){
-		$contact.css({
-			"z-index":9999,
-			"opacity":1,
-		});
-	});
-	$grid4.click(function(){
-		$favourites.css({
-			"z-index":9999,
-			"opacity":1,
-		});
-	});
-	$close.click(function(){
-		$contact.add($favourites).css({
-			"z-index":-1,
-			"opacity":0,
-		});
-	});
-
+  
 });
